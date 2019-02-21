6f69e3dfae3389b917506848f959d73011fbcd24
Zhijia Chen <zhijia.chen@temple.edu>
2019-02-21 15:54:15 -0500

    fix run.py to catch process id of mininet nodes

--- run.py
+++ run.py
@@ -16,7 +16,7 @@ parser.add_argument('--cmd', default='ifconfig',
                     help="Command to run inside node.")
 
 FLAGS = parser.parse_args()
-node_pat = re.compile(r'.*bash ... mininet:(.*)')
+node_pat = re.compile(r'.*mininet:(.*)')
 
 
 def list_nodes(do_print=False):
